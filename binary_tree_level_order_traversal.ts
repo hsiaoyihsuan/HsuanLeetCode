52dd1467f9e862f447f330eb825fcd5f49ec3c8a
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-04-19 15:46:35 +0800

    Add 102. Binary Tree Level Order Traversal TypeScript

---
+++ binary_tree_level_order_traversal.ts
@@ -0,0 +1,35 @@
+// 102. Binary Tree Level Order Traversal
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return [];
+
+  const result: number[][] = [];
+  const queue: TreeNode[] = [root];
+
+  while (queue.length > 0) {
+    const level: number[] = [];
+    const queueSize = queue.length;
+    for (let i = 0; i < queueSize; i++) {
+      const node = queue.shift();
+      level.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+  }
+
+  return result;
+}
