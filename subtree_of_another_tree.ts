93550ab81f711b1654b7f91fc9ef95a64ff3f20b
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-04-20 14:07:17 +0800

    Add 572. Subtree of Another Tree TypeScript

---
+++ subtree_of_another_tree.ts
@@ -0,0 +1,30 @@
+// 572. Subtree of Another Tree
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if (!root || !subRoot) return false;
+
+  if (isSameTree(root, subRoot)) return true;
+
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
+
+function isSameTree(root: TreeNode, subRoot: TreeNode): boolean {
+  if (!root || !subRoot) return root === subRoot;
+  if (root.val !== subRoot.val) return false;
+  return (
+    isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right)
+  );
+}
