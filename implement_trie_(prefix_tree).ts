b38814cad94518a7bb098204de11a757469173c4
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2025-04-26 14:09:43 +0800

    Add 208. Implement Trie (Prefix Tree) TypeScript

---
+++ implement_trie_(prefix_tree).ts
@@ -0,0 +1,69 @@
+// 208. Implement Trie (Prefix Tree)
+// Time: O(n) n is the length of the word
+// Space: O(n) for insert, O(1) for search and startsWith
+// Build the trie data structure, each node contains a map of children (a-z) and a boolean to indicate if it's the end of a word.
+class TrieNode {
+  children: Map<string, TrieNode>;
+  isEnd: boolean;
+
+  constructor() {
+    this.children = new Map();
+    this.isEnd = false;
+  }
+}
+
+class Trie {
+  private root;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word: string): void {
+    const chars = word.split("");
+
+    let node = this.root;
+    for (const char of chars) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode());
+      }
+
+      node = node.children.get(char);
+    }
+    node.isEnd = true;
+  }
+
+  search(word: string): boolean {
+    const chars = word.split("");
+
+    let node = this.root;
+    for (const char of chars) {
+      if (!node.children.has(char)) return false;
+
+      node = node.children.get(char);
+    }
+
+    return node.isEnd;
+  }
+
+  startsWith(prefix: string): boolean {
+    const chars = prefix.split("");
+
+    let node = this.root;
+    for (const char of chars) {
+      if (!node.children.has(char)) return false;
+
+      node = node.children.get(char);
+    }
+
+    return true;
+  }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
