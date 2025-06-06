37dc118f2d6927cdfe471c4dbe7a4e699c05f479
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2025-06-07 01:22:57 +0800

    Add 57. Insert Interval TypeScript

---
+++ insert_interval.ts
@@ -0,0 +1,24 @@
+// 57. Insert Interval
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  const result: number[][] = [];
+
+  let inserted = false;
+  for (const [start, end] of intervals) {
+    if (end < newInterval[0]) {
+      result.push([start, end]);
+    } else if (start > newInterval[1]) {
+      if (!inserted) {
+        result.push(newInterval);
+        inserted = true;
+      }
+      result.push([start, end]);
+    } else {
+      newInterval[0] = Math.min(start, newInterval[0]);
+      newInterval[1] = Math.max(end, newInterval[1]);
+    }
+  }
+
+  if (!inserted) result.push(newInterval);
+
+  return result;
+}
