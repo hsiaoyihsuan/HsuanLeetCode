941d87adcd39f4595304d07003b00d1ae9ed781c
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-07-12 16:21:08 +0800

    Refactor MinStack2 push method for clarity and add MinStack3 implementation

--- min_stack.ts
+++ min_stack.ts
@@ -54,7 +54,9 @@ class MinStack2 {
 
   push(val: number): void {
     this.stack.push(val);
-    this.minStack.push(Math.min(this.minStack.at(-1)!, val));
+    const min =
+      this.minStack.length === 0 ? val : Math.min(this.minStack.at(-1)!, val);
+    this.minStack.push(min);
   }
 
   pop(): void {
@@ -70,6 +72,49 @@ class MinStack2 {
     return this.minStack.at(-1)!;
   }
 }
+
+// Another alternative implementation using a single stack with a trick to store the minimum
+class MinStack3 {
+  private stack: number[];
+  private min = Infinity;
+
+  constructor() {
+    this.stack = [];
+  }
+
+  push(val: number): void {
+    if (this.stack.length === 0) {
+      this.stack.push(0);
+      this.min = val;
+    } else {
+      this.stack.push(val - this.min);
+      this.min = Math.min(this.min, val);
+    }
+  }
+
+  pop(): void {
+    const pop = this.stack.pop()!;
+    if (pop < 0) this.min -= pop;
+  }
+
+  top(): number {
+    const top = this.stack.at(-1)!;
+    return top < 0 ? this.min : this.min + top;
+  }
+
+  getMin(): number {
+    return this.min;
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
 
 /**
  * Your MinStack object will be instantiated and called as such:
