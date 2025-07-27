e1b77c07f1d6e9632fd1a74e428bda1abbda1d5d
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-07-27 15:46:31 +0800

    Add mergeTwoLists function with iterative and recursive implementations

---
+++ merge_two_sorted_lists.ts
@@ -0,0 +1,51 @@
+// 21. Merge Two Sorted Lists
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// Iteration in place
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  const dummy: ListNode | null = new ListNode();
+
+  let current = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1) current.next = list1;
+  if (list2) current.next = list2;
+
+  return dummy.next;
+}
+
+// Recursive in place
+function mergeTwoLists2(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+}
