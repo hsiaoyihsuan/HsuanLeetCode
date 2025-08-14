a713ab50139100f847e99509164d3da0bc98844d
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2025-08-15 00:38:38 +0800

    Refactor lengthOfLongestSubstring function to improve performance and clarity

--- longest_substring_without_repeating_charaters.ts
+++ longest_substring_without_repeating_charaters.ts
@@ -1,22 +1,6 @@
 // 3. Longest Substring Without Repeating Characters
-// Time: O(n2)
-function lengthOfLongestSubstringV1(s: string): number {
-  let result: number = 0;
-  for (let i = 0; i < s.length; i++) {
-    const set = new Set();
-    for (let j = i; j < s.length; j++) {
-      if (!set.has(s[j])) {
-        set.add(s[j]);
-      } else {
-        break;
-      }
-    }
-    result = Math.max(result, set.size);
-  }
-  return result;
-}
-
-// Sliding Window. Time: O(n), Space: O(n)
+// Sliding Window.
+// Time: O(n), Space: O(n)
 function lengthOfLongestSubstringV2(s: string): number {
   const set = new Set<string>();
   let result = 0;
