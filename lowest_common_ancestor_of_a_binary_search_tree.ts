764c4cb441669ea91747d6c393cd1fb957b1b262
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-08-31 00:12:15 +0800

    Refactor lowest common ancestor implementation for improved clarity and consistency

--- lowest_common_ancestor_of_a_binary_search_tree.ts
+++ lowest_common_ancestor_of_a_binary_search_tree.ts
@@ -1,35 +1,34 @@
 // 235. Lowest Common Ancestor of a Binary Search Tree
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
-// Time Complexity: O(h), where h is the height of the tree
+// Recursive
+// Time: O(h), Space: O(h)
 function lowestCommonAncestor(
   root: TreeNode | null,
   p: TreeNode | null,
   q: TreeNode | null
 ): TreeNode | null {
-  if (p.val < root.val && q.val < root.val) {
-    return lowestCommonAncestor(root.left, p, q);
-  } else if (p.val > root.val && q.val > root.val) {
-    return lowestCommonAncestor(root.right, p, q);
+  if (p!.val < root!.val && q!.val < root!.val) {
+    return lowestCommonAncestor(root!.left, p, q);
+  } else if (p!.val > root!.val && q!.val > root!.val) {
+    return lowestCommonAncestor(root!.right, p, q);
   } else {
     return root;
   }
 }
 
-// This one is an iterative solution which is faster
-function lowestCommonAncestor(
+// Iteragive
+// Time: O(h), Space: O(1)
+function lowestCommonAncestor2(
   root: TreeNode | null,
   p: TreeNode | null,
   q: TreeNode | null
@@ -37,9 +36,9 @@ function lowestCommonAncestor(
   let current = root;
 
   while (current) {
-    if (p.val < current.val && q.val < current.val) {
+    if (p!.val < current.val && q!.val < current.val) {
       current = current.left;
-    } else if (p.val > current.val && q.val > current.val) {
+    } else if (p!.val > current.val && q!.val > current.val) {
       current = current.right;
     } else {
       return current;
