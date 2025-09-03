5f6ca84d45aeb66ed152c8bfc1135806ac580361
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-09-03 22:43:09 +0800

    Refactor TreeNode class definition for improved clarity and consistency

--- binary_tree_level_order_traversal.ts
+++ binary_tree_level_order_traversal.ts
@@ -1,18 +1,17 @@
 // 102. Binary Tree Level Order Traversal
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
+// BFS
+// Time: O(n), Space: O(w)
 function levelOrder(root: TreeNode | null): number[][] {
   if (!root) return [];
 
@@ -23,7 +22,7 @@ function levelOrder(root: TreeNode | null): number[][] {
     const level: number[] = [];
     const queueSize = queue.length;
     for (let i = 0; i < queueSize; i++) {
-      const node = queue.shift();
+      const node = queue.shift()!;
       level.push(node.val);
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
