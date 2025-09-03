569ead95e1f1f7bb1bcc7eb130d96b9ad18ca8f2
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2025-09-03 23:11:09 +0800

    Add 199. Binary Tree Right Side View TypeScript

---
+++ binary_tree_right_side_view.ts
@@ -0,0 +1,30 @@
+// 199. Binary Tree Right Side View
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function rightSideView(root: TreeNode | null): number[] {
+  if (!root) return [];
+
+  const result: number[] = [];
+  const queue: TreeNode[] = [root];
+  while (queue.length > 0) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift()!;
+      if (i === size - 1) {
+        result.push(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return result;
+}
