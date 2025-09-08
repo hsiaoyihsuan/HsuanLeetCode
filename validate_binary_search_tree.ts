87815c330591d67cfe616848d772d6ecf7d87703
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-09-08 22:55:47 +0800

    Refactor 98. Validate Binary Search Tree

--- validate_binary_search_tree.ts
+++ validate_binary_search_tree.ts
@@ -1,19 +1,17 @@
 // 98. Validate Binary Search Tree
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
-// Time: O(n). Space: O(h). Preorder Traversal.
+// DFS. Preorder Traversal
+// Time: O(n), Space: O(h)
 function isValidBST(root: TreeNode | null): boolean {
   function isValid(node: TreeNode | null, left: number, right: number) {
     if (!node) return true;
@@ -26,4 +24,22 @@ function isValidBST(root: TreeNode | null): boolean {
   }
 
   return isValid(root, -Infinity, Infinity);
+}
+
+// BFS
+// Time: O(n), Space: O(h)
+function isValidBST2(root: TreeNode | null) {
+  if (!root) return true;
+
+  const queue = [{node: root, left: -Infinity, right: Infinity}];
+
+  while (queue.length > 0) {
+    const {node, left, right} = queue.shift()!;
+    if (node.val <= left || node.val >= right) return false;
+
+    if (node.left) queue.push({node: node.left, left, right: node.val});
+    if (node.right) queue.push({node: node.right, left: node.val, right});
+  }
+
+  return true;
 }
