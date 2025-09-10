cca15f535346e67f26eeb022624ab7987cac250c
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2025-09-10 23:17:14 +0800

    Refactor 105. Construct Binary Tree from Preorder and Inorder Traversal

--- construct_binary_tree_from_preorder_and_inorder_traversal.ts
+++ construct_binary_tree_from_preorder_and_inorder_traversal.ts
@@ -1,18 +1,17 @@
 // 105. Construct Binary Tree from Preorder and Inorder Traversal
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
+// DFS
+// TIme: O(n^2), Space: O(n)
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
   if (!preorder.length || !inorder.length) return null;
 
@@ -28,4 +27,28 @@ function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
   );
 
   return new TreeNode(rootVal, leftTree, rightTree);
+}
+
+// DFS + Hash Map
+// Time: O(n), Space: O(n)
+function buildTree2(preorder: number[], inorder: number[]) {
+  const map = new Map();
+  inorder.forEach((val, i) => map.set(val, i));
+
+  let preIndex = 0;
+
+  function dfs(left: number, right: number): TreeNode | null {
+    if (left > right) return null;
+
+    const rootVal = preorder[preIndex++];
+    const root = new TreeNode(rootVal);
+
+    const mid = map.get(rootVal);
+    root.left = dfs(left, mid - 1);
+    root.right = dfs(mid + 1, right);
+
+    return root;
+  }
+
+  return dfs(0, preorder.length - 1);
 }
