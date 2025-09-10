973a74d5cc28ca45d9d62c6339ab41b695dec7d4
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-09-10 23:03:26 +0800

    Refactor 124. Binary Tree Maximum Path Sum

--- binary_tree_maximum_path_sum.ts
+++ binary_tree_maximum_path_sum.ts
@@ -1,26 +1,23 @@
 // 124. Binary Tree Maximum Path Sum
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
 // Time & Space: O(n)
 // Use DFS to calculate the left and right subtree's max value
-// We can decide if with split at the current not or not
+// We can decide if with split at the current node or not
 // The subtree should only return the max value without split
 function maxPathSum(root: TreeNode | null): number {
   let result = -Infinity;
 
-  function dfs(node: TreeNode | null) {
+  function dfs(node: TreeNode | null): number {
     if (node === null) return 0;
 
     // Get max path sum from left and right children (ignore negatives)
