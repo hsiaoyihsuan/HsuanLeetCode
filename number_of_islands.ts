51383084fdb64ff7d2e72f3e13c89202511d947d
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2025-12-01 10:12:55 +0800

    feat: Add DFS solution to 200. Number of Islands

--- number_of_islands.ts
+++ number_of_islands.ts
@@ -1,44 +1,80 @@
 // 200. Number of Islands
-// BFS. Time: O(m x n). Space: O(min(m x n)).
+
+// BFS. Time: O(m x n). Space: O(m x n).
 function numIslands(grid: string[][]): number {
-  if (!grid || grid.length === 0) return 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
 
-  let result = 0;
-  let rows = grid.length;
-  let cols = grid[0].length;
-
-  function bfs(grid: string[][], i: number, j: number): void {
-    const queue: [number, number][] = [[i, j]];
-    grid[i][j] = "0"; // visited
+  function bfs(startR: number, startC: number) {
+    const queue: [number, number][] = [[startR, startC]];
+    grid[startR][startC] = "0"; // mark as visited
 
     while (queue.length > 0) {
-      const [x, y] = queue.shift()!;
-      for (const [dx, dy] of [
+      const [r, c] = queue.shift()!;
+
+      for (const [dr, dc] of [
         [1, 0],
         [-1, 0],
         [0, 1],
         [0, -1],
       ]) {
-        const newX = x + dx;
-        const newY = y + dy;
-        if (newX < 0 || newY < 0 || newX >= rows || newY >= cols) continue;
+        const nr = r + dr;
+        const nc = c + dc;
 
-        if (grid[newX][newY] === "1") {
-          queue.push([newX, newY]);
-          grid[newX][newY] = "0";
+        if (
+          nr >= 0 &&
+          nc >= 0 &&
+          nr < rows &&
+          nc < cols &&
+          grid[nr][nc] === "1"
+        ) {
+          queue.push([nr, nc]);
+          grid[nr][nc] = "0";
         }
       }
     }
   }
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        result++;
-        bfs(grid, i, j);
+  let islands = 0;
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === "1") {
+        islands++;
+        bfs(r, c);
       }
     }
   }
 
-  return result;
+  return islands;
+}
+
+// DFS
+// Time: O(m x n), Space: O(m x n)
+function numIslandsDFS(grid: string[][]): number {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  function dfs(r: number, c: number) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === "0") {
+      return;
+    }
+
+    grid[r][c] = "0";
+    dfs(r + 1, c);
+    dfs(r - 1, c);
+    dfs(r, c + 1);
+    dfs(r, c - 1);
+  }
+
+  let count = 0;
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === "1") {
+        count++;
+        dfs(r, c);
+      }
+    }
+  }
+  return count;
 }
