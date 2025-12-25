45d12e66e86f1626846113210b4af5d43cd4c797
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2025-12-25 15:40:25 +0800

    chore: Add time and space analysis to question 56

--- merge_intervals.ts
+++ merge_intervals.ts
@@ -1,10 +1,12 @@
 // 56. Merge Intervals
+// Time: O(n log n)
+// Space: O(1) or O(n) depending on the sorting algorithm
 function merge(intervals: number[][]): number[][] {
   intervals.sort((a, b) => a[0] - b[0]);
   const result: number[][] = [intervals[0]];
 
   for (let i = 1; i < intervals.length; i++) {
-    const last = result.at(-1);
+    const last = result.at(-1)!;
     const curr = intervals[i];
 
     if (curr[0] <= last[1]) {
