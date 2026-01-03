a25072369fddb540567f717ef7b73db60fd2e680
HSUAN HSIAO <HSUAN.HSIAO@trusted-sol.com>
2026-01-03 16:24:36 +0800

    chore: refine solution for 355. Design Twitter

--- design_twitter.ts
+++ design_twitter.ts
@@ -67,66 +67,67 @@ class Twitter {
 
 import {MaxPriorityQueue} from "@datastructures-js/priority-queue";
 class Twitter2 {
-  private users: Map<
-    number,
-    {
-      tweets: {id: number; time: number}[];
-      followees: Set<number>;
-    }
-  >;
-  private tweetCount: number;
+  private userPost: Map<number, [number, number][]>;
+  private userFollow: Map<number, Set<number>>;
+  private time: number;
 
   constructor() {
-    this.users = new Map();
-    this.tweetCount = 0;
+    this.userPost = new Map();
+    this.userFollow = new Map();
+    this.time = 0;
   }
 
   postTweet(userId: number, tweetId: number): void {
-    if (!this.users.has(userId)) {
-      this.users.set(userId, {tweets: [], followees: new Set()});
-    }
-    const user = this.users.get(userId)!;
-    user.tweets.push({id: tweetId, time: this.tweetCount++});
+    if (!this.userPost.has(userId)) this.userPost.set(userId, []);
+
+    this.userPost.get(userId)!.push([this.time++, tweetId]);
   }
 
   getNewsFeed(userId: number): number[] {
-    if (!this.users.has(userId)) return [];
+    if (!this.userFollow.has(userId)) this.userFollow.set(userId, new Set());
 
-    const user = this.users.get(userId)!;
-    const maxHeap = new MaxPriorityQueue<{id: number; time: number}>(
-      (tweet) => tweet.time
+    this.userFollow.get(userId)!.add(userId);
+
+    // [time, tweetId, followeeId, currentIndex]
+    const maxHeap = new MaxPriorityQueue<[number, number, number, number]>(
+      (item) => item[0]
     );
-    for (const tweet of user.tweets) {
-      maxHeap.enqueue(tweet);
-    }
 
-    for (const followeeId of user.followees) {
-      if (this.users.has(followeeId)) {
-        const followee = this.users.get(followeeId)!;
-        followee.tweets.forEach((tweet) => maxHeap.enqueue(tweet));
+    for (const followeeId of this.userFollow.get(userId)!) {
+      const tweets = this.userPost.get(followeeId);
+      if (tweets && tweets.length > 0) {
+        const index = tweets.length - 1;
+        const [time, tweetId] = tweets.at(-1)!;
+        maxHeap.push([time, tweetId, followeeId, index]);
       }
     }
 
-    const feed: number[] = [];
-    while (maxHeap.size() > 0 && feed.length < 10) {
-      const {id} = maxHeap.dequeue()!;
-      feed.push(id);
+    const result: number[] = [];
+
+    while (maxHeap.size() > 0 && result.length < 10) {
+      const [time, tweetId, followeeId, index] = maxHeap.pop()!;
+      result.push(tweetId);
+
+      if (index - 1 >= 0) {
+        const tweets = this.userPost.get(followeeId)!;
+        const [time, tweetId] = tweets[index - 1];
+        maxHeap.push([time, tweetId, followeeId, index - 1]);
+      }
     }
-    return feed;
+
+    return result;
   }
 
   follow(followerId: number, followeeId: number): void {
-    if (!this.users.has(followerId)) {
-      this.users.set(followerId, {tweets: [], followees: new Set()});
-    }
-    const user = this.users.get(followerId)!;
-    user.followees.add(followeeId);
+    if (!this.userFollow.has(followerId))
+      this.userFollow.set(followerId, new Set());
+
+    this.userFollow.get(followerId)!.add(followeeId);
   }
 
   unfollow(followerId: number, followeeId: number): void {
-    if (this.users.has(followerId)) {
-      const user = this.users.get(followerId)!;
-      user.followees.delete(followeeId);
+    if (this.userFollow.has(followerId)) {
+      this.userFollow.get(followerId)!.delete(followeeId);
     }
   }
 }
