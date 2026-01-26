788352f1095151c70da4ac8942e072730bcfaf11
YI-HSUAN HSIAO <hsuan9705@gmail.com>
2026-01-26 23:44:16 +0800

    chore: add complexity to 53. Maximum Subarray

--- maximum_subarray.ts
+++ maximum_subarray.ts
@@ -1,24 +1,12 @@
 // 53. Maximum Subarray
-// Version 1: Brute force solution
-function maxSubArrayV1(nums: number[]): number {
-  const sums: number[] = [];
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      sums.push(nums.slice(i, j + 1).reduce((a, b) => a + b, 0));
-    }
-  }
-  return Math.max(...sums);
-}
-
-// Version 2: Kadane's Algorithm
+// Kadane's Algorithm
+// Time: O(n), Space: O(1)
 function maxSubArrayV2(nums: number[]): number {
-  let currentMax = nums[0];
-  let globalMax = nums[0];
+  let curMax = nums[0];
+  let result = nums[0];
   for (let i = 1; i < nums.length; i++) {
-    currentMax = Math.max(currentMax + nums[i], nums[i]);
-    if (currentMax > globalMax) {
-      globalMax = currentMax;
-    }
+    curMax = Math.max(curMax + nums[i], nums[i]);
+    result = Math.max(result, curMax);
   }
-  return globalMax;
+  return result;
 }
